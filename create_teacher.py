7e70b489bf02224a499791de86dcfded811d940d
kennyslis <3283712565@qq.com>
2026-03-02 17:18:10 +0800

    Revert "april_2th"
    This reverts commit 55b2ecbf61d2ae996a03c006314f83bdacb5f63c.

--- create_teacher.py
+++ create_teacher.py
@@ -1,8 +1,9 @@
 import os
 import django
+from django.conf import settings
 
 # 设置 Django 环境变量
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_assignment_system.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "assignment_system.settings")  # 替换为你的项目设置模块
 django.setup()
 
 from assignment_system.models import CustomUser  # 替换为你的应用名
